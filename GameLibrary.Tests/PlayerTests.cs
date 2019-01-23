0766798c09a649f74fc32704e9b0a98f67425aa0
Joshua Morris <jrem94@live.com>
2019-01-23 17:14:04 -0600

    Initial Commit

---
+++ GameLibrary.Tests/PlayerTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameLibrary;
+using Xunit;
+
+namespace GameLibrary.Tests
+{
+    public class PlayerTests
+    {
+        [Fact]
+        public void GetPlayerHealth()
+        {
+            Player player = new Player(100);
+
+            int actual = player.GetHealth();
+
+            Assert.Equal(100, actual);
+        }
+
+        [Fact]
+        public void DamagePlayer()
+        {
+            Player player = new Player(100);
+
+            player.TakeDamage(25);
+
+            Assert.Equal(75, player.GetHealth());
+        }
+
+        [Fact]
+        public void CheckPlayerAlive()
+        {
+            Player player = new Player(100);
+
+            Assert.True(player.IsAlive());
+        }
+
+        [Fact]
+        public void CheckPlayerDead1()
+        {
+            Player player = new Player(100);
+
+            player.TakeDamage(100);
+
+            Assert.False(player.IsAlive());
+        }
+
+        [Fact]
+        public void CheckPlayerDead2()
+        {
+            Player player = new Player(100);
+
+            player.TakeDamage(150);
+
+            Assert.False(player.IsAlive());
+        }
+    }
+
+    public class ItemTests
+    {
+
+    }
+
+    public class AbilityTests
+    {
+
+    }
+
+    public class FoeTests
+    {
+
+    }
+
+    public class EncounterGenerator
+    {
+
+    }
+}
---
+++ GameLibrary/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public class Player
+    {
+        private int health;
+
+        public Player(int health)
+        {
+            this.health = health;
+        }
+
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            this.health -= damage;
+        }
+
+        public bool IsAlive()
+        {
+            if (this.health <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
